bacce76e6e91beefe2eb56ea8d3e8c2e9bcbdce2
dobsonmr <mdobson2@nd.edu>
2025-07-12 20:01:40 +0000

    Deploying to gh-pages from @ dobsonmr/MackenzieRDobson@702640463c4a35c0c981f62a0afc526b3b19e06a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/MackenzieRDobson/";
           },
+        },{id: "nav-publications",
+          title: "Publications",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/MackenzieRDobson/research/publications/";
+          },
         },{id: "nav-research",
           title: "Research",
           description: "",
