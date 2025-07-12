5e4cde9e7a7a0f7c6ac633858c6bf1355e406b9c
dobsonmr <mdobson2@nd.edu>
2025-07-12 13:51:07 +0000

    Deploying to gh-pages from @ dobsonmr/MackenzieRDobson@f2e88f76a3f4328360bdc401465d0b59a2d919cf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -38,8 +38,8 @@ ninja.data = [{
             window.location.href = "/MackenzieRDobson/repositories/";
           },
         },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          title: "CV",
+          description: "This is a description of the CV page. You can also change or remove the PDF download button.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/MackenzieRDobson/cv/";
