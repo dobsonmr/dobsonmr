1844e4b53c288bd67ba3f420c246551f710c07e3
dobsonmr <mdobson2@nd.edu>
2025-07-13 16:10:19 +0000

    Deploying to gh-pages from @ dobsonmr/MackenzieRDobson@10ad44183dfeb87a4d1c0a7ad11eb3ef23656ecb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -7,34 +7,34 @@ ninja.data = [{
     title: "About",
     section: "Navigation",
     handler: () => {
-      window.location.href = "/MackenzieRDobson/";
+      window.location.href = "/";
     },
   },{id: "nav-research",
           title: "Research",
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/MackenzieRDobson/research/";
+            window.location.href = "/research/";
           },
         },{id: "nav-teaching",
           title: "Teaching",
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/MackenzieRDobson/teaching/";
+            window.location.href = "/teaching/";
           },
         },{id: "nav-cv",
           title: "CV",
           description: "Below you can find information on my institutional affiliations, publications, and manuscripts under review. For additional information, view my complete CV by clicking the PDF icon.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/MackenzieRDobson/cv/";
+            window.location.href = "/cv/";
           },
         },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
           section: "Books",handler: () => {
-              window.location.href = "/MackenzieRDobson/books/the_godfather/";
+              window.location.href = "/books/the_godfather/";
             },},{id: "news-a-simple-inline-announcement",
           title: 'A simple inline announcement.',
           description: "",
@@ -42,7 +42,7 @@ ninja.data = [{
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/MackenzieRDobson/news/announcement_2/";
+              window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
@@ -50,52 +50,52 @@ ninja.data = [{
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
-              window.location.href = "/MackenzieRDobson/projects/1_project/";
+              window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
           title: 'project 2',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
-              window.location.href = "/MackenzieRDobson/projects/2_project/";
+              window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
           title: 'project 3 with very long name',
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
-              window.location.href = "/MackenzieRDobson/projects/3_project/";
+              window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
           title: 'project 4',
           description: "another without an image",
           section: "Projects",handler: () => {
-              window.location.href = "/MackenzieRDobson/projects/4_project/";
+              window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
           title: 'project 5',
           description: "a project with a background image",
           section: "Projects",handler: () => {
-              window.location.href = "/MackenzieRDobson/projects/5_project/";
+              window.location.href = "/projects/5_project/";
             },},{id: "projects-project-6",
           title: 'project 6',
           description: "a project with no image",
           section: "Projects",handler: () => {
-              window.location.href = "/MackenzieRDobson/projects/6_project/";
+              window.location.href = "/projects/6_project/";
             },},{id: "projects-project-7",
           title: 'project 7',
           description: "with background image",
           section: "Projects",handler: () => {
-              window.location.href = "/MackenzieRDobson/projects/7_project/";
+              window.location.href = "/projects/7_project/";
             },},{id: "projects-project-8",
           title: 'project 8',
           description: "an other project with a background image and giscus comments",
           section: "Projects",handler: () => {
-              window.location.href = "/MackenzieRDobson/projects/8_project/";
+              window.location.href = "/projects/8_project/";
             },},{id: "projects-project-9",
           title: 'project 9',
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
-              window.location.href = "/MackenzieRDobson/projects/9_project/";
+              window.location.href = "/projects/9_project/";
             },},{id: "publications-i-39-m-coming-out-how-voter-discrimination-produces-effective-lgbtq-lawmakers",
           title: 'I&amp;#39;m Coming Out! How Voter Discrimination Produces Effective LGBTQ Lawmakers',
           description: "",
           section: "Publications",handler: () => {
-              window.location.href = "/MackenzieRDobson/publication/coming-out/";
+              window.location.href = "/publication/coming-out/";
             },},{
       id: 'light-theme',
       title: 'Change theme to light',
